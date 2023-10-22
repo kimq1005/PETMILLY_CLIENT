7108102e4e1b49d33cde6d212d85a8ddad6c09d9
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2023-10-22 18:52:22 +0900

    Modify | - 텍스트 변경 - import 변경

--- app/src/main/java/com/llama/petmilly_client/presentation/signupscreen/SignUpScreen_4_2_CallYourAnimal.kt
+++ app/src/main/java/com/llama/petmilly_client/presentation/signupscreen/SignUpScreen_4_2_CallYourAnimal.kt
@@ -1,45 +1,38 @@
 package com.llama.petmilly_client.presentation.signupscreen
 
-import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.llama.petmilly_client.R
 import com.llama.petmilly_client.presentation.shelterscreen.TitleBar
 import com.llama.petmilly_client.presentation.signupscreen.viewmodel.SignUpViewModel
 import com.llama.petmilly_client.ui.theme.Button_Clicked
 import com.llama.petmilly_client.ui.theme.Button_NoneClicked
 import com.llama.petmilly_client.ui.theme.Category_Cliked
-import com.llama.petmilly_client.ui.theme.TextField_BackgroudColor
 import com.llama.petmilly_client.utils.*
-import llama.test.jetpack_dagger_plz.utils.Common
 import llama.test.jetpack_dagger_plz.utils.Common.SIGNUPSCREEN_4_3_CALLYOUTANIMAL_First
-import llama.test.jetpack_dagger_plz.utils.Common.SIGNUPSCREEN_5_ISTEMPORARYCARE
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
@@ -49,7 +42,9 @@ fun SignUpScreen_4_2_CallYourAnimal(navController: NavController, viewModel: Sig
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
 
-    Column(modifier = Modifier.fillMaxSize().background(color = Color.White)) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.White)) {
         TitleBar(title = "", ismenu = false, clickBack = {
             navController.popBackStack()
         }) {
@@ -147,7 +142,7 @@ fun SignUpScreen_4_2_CallYourAnimal(navController: NavController, viewModel: Sig
 
             ButtonShapeScreen(
                 title = "3마리와 함께 살고 있어요.",
-                textcolor = if (viewModel.numberofanimal.value == "3마리와 함께 살고 있어요.") Color.White else Color.Black,
+                textcolor = if (viewModel.numberofanimal.value == stringResource(id = R.string.test_string)) Color.White else Color.Black,
                 fontSize = 15,
                 modifier = Modifier
                     .padding(start = 10.dp)
